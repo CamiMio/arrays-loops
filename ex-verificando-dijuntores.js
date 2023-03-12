09d56c51c6db1d9cbb7dcf7771715bc3acd50421
Camila Mio <camilamio25@gmail.com>
2023-03-12 11:46:31 -0300

    verificando true e false

---
+++ ex-verificando-dijuntores.js
@@ -0,0 +1,24 @@
+
+
+const disjuntores = [
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ];
+
+for (let i = 0; i < disjuntores.length; i++) {
+    const element = disjuntores[i];
+
+    if(element === true){
+        console.log(i)
+
+    }
+    
+}
